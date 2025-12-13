6f79bafb2c63e4a4723210b08f7f795ce8905c68
Bakhtawer Jabeen <bakhtawerhashmi@gmail.com>
2025-12-14 02:04:38 +0500

    client deleted

--- client/context/UserContext.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-import { User } from '../types';
-
-export const UserContext = React.createContext<{ user: User | null; logout: () => void }>({ 
-    user: null, 
-    logout: () => {} 
-});
