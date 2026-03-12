3c18154c1d1f45c7c81f64061b647a99ce1c69fe
Bakhtawer Jabeen <bakhtawerhashmi@gmail.com>
2026-03-12 23:20:08 +0500

    added autorefixer ad postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,5 @@
+// PostCSS configuration isn't needed in production because styles are
+// loaded via CDN (Tailwind). Keeping an empty export so Vite doesn't error.
 export default {
-  plugins: {
-    autoprefixer: {},
-  },
-}
+  plugins: {},
+};
